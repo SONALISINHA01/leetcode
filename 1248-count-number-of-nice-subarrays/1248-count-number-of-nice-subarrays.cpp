83d23316e3bed8ec4a5a58caf6964eddc46f60cf
SONALISINHA01 <149989109+SONALISINHA01@users.noreply.github.com>
2026-02-04 14:44:45 +0530

    Time: 12 ms (40.87%), Space: 71.3 MB (99.73%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
@@ -5,14 +5,11 @@ public:
         int si = 0, ei = 0;
         int oddCount = 0;
         int subarraycount = 0;
-
         while(ei < n){
             if(nums[ei++] % 2 == 1) oddCount++;
-
             while(oddCount > k){
                 if(nums[si++] % 2 == 1) oddCount--;
             }
-
             subarraycount += ei - si;
         }
         return subarraycount;
