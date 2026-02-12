75070a87100c7feb0771c3e74a0a41bf3c1919a9
SONALISINHA01 <149989109+SONALISINHA01@users.noreply.github.com>
2026-02-12 16:17:21 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     int climbStairs(int n) {
         vector<int> dparr(n+1,0);
-        dparr[0]=1;
         dparr[1]=1;
-        for(int i=2;i<=n;i++){
+        dparr[2]=2;
+        for(int i=3;i<=n;i++){
             dparr[i]=dparr[i-1]+dparr[i-2];
         }
         return dparr[n];
