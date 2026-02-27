b7168db36d5fe3ad3a23538acb8b2d71ed8f99b0
SONALISINHA01 <149989109+SONALISINHA01@users.noreply.github.com>
2026-02-27 09:31:49 +0530

    Time: 0 ms (100%), Space: 14.7 MB (41.72%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.cpp
+++ 0841-keys-and-rooms/0841-keys-and-rooms.cpp
@@ -1,33 +1,34 @@
 class Solution {
 public:
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
-        int n = rooms.size();
-        vector<int> locrooms(n, 1);
 
-        // for (int i = 0; i < n; i++) {
-        //     int sz = rooms[i].size();
-        //     for (int j = 0; j < sz; j++) {
-        //         locrooms[rooms[i][j]] = 1;
-        //     }
-        // }
+        int n = rooms.size();
+        vector<int> locrooms(n,1);
+
         queue<int> q;
-        for (int i = 0; i < rooms[0].size(); i++) {
-            locrooms[rooms[0][i]] = 0;
+
+        int sz  = rooms[0].size();
+        locrooms[0]=0;
+        for(int i =0;i<sz;i++){
             q.push(rooms[0][i]);
+            locrooms[rooms[0][i]]=0;
         }
-        while (!q.empty()) {
-            int i = q.front();
+
+        while(!q.empty()){
+
+            int key = q.front();
             q.pop();
-            for (int j = 0; j < rooms[i].size(); j++) {
-                if (locrooms[rooms[i][j]] == 1) {
-                    q.push(rooms[i][j]);
-                    locrooms[rooms[i][j]] = 0;
+            int sze = rooms[key].size();
+
+            for(int i=0;i<sze;i++){
+                if(locrooms[rooms[key][i]]==1){
+                    q.push(rooms[key][i]);
+                    locrooms[rooms[key][i]]=0;
                 }
             }
         }
-        locrooms[0]=0;
-        for (int i = 0; i < n; i++) {
-            if (locrooms[i] == 1) {
+        for(int i =0;i<n;i++){
+            if(locrooms[i]==1){
                 return false;
             }
         }
