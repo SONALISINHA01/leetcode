531701eadee23e3841d23506ab3e06329a47f87b
SONALISINHA01 <149989109+SONALISINHA01@users.noreply.github.com>
2026-05-10 20:56:18 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,26 +1,35 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
+        int n = s.size(), m = t.size();
         vector<int> freq(128,0);
-        for(auto c:t) freq[c]++;
-        int minlen=INT_MAX, start=0;
-        int count =t.size(), left =0;
-        for(int right = 0;right<s.size();right++){
-            if(freq[s[right]]>0)count--;
-            freq[s[right]]--;
-            while(count==0){
-                if(right-left +1<minlen){
-                    minlen = right-left+1;
-                    start = left;
-                }
-                freq[s[left]]++;
-                if(freq[s[left]]>0)count++;
-                left++;
+        for(int i=0;i<m;i++){
+            freq[t[i]]++;
+        }
+        int count = m;
+        int ans = INT_MAX;
+        int en =0, st=0;
+        int start =0;
+        while(en<n){
+            freq[s[en]]--;
+            if(freq[s[en]]>=0){
+                count--;
             }
+            while(count==0){
+                if(ans>(en-st+1)){
+                    ans = (en-st+1);
+                    start = st;
+                }
+                
+                freq[s[st]]++;
+                if(freq[s[st]]>0){
+                    count++;
+                }
+                st++;
+            }
+            en++;
         }
-        if(minlen!=INT_MAX){
-            return s.substr(start,minlen);
-        }
-        return "";
+        if(ans==INT_MAX)return "";
+        return s.substr(start,ans);
     }
 };
