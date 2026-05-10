51a1c29315efc7cd7718bbbc1034bc9fd87021f9
SONALISINHA01 <149989109+SONALISINHA01@users.noreply.github.com>
2026-05-11 00:49:24 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -2,30 +2,29 @@ class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
         int n = s1.size(), m = s2.size();
-        vector<int> freq(256,0);
+        vector<int> freq(256, 0);
         int count = n;
-        for(int i=0;i<n;i++){
+        for (int i = 0; i < n; i++) {
             freq[s1[i]]++;
         }
-        int st =0, en =0;
-        while(en<m){
+        int st = 0, en = 0;
+        while (en < m) {
             freq[s2[en]]--;
-            if(freq[s2[en]]>=0){
+            if (freq[s2[en]] >= 0) {
                 count--;
             }
-            if(count==0){
-                
-                    while(count==0){
-                        if(en-st+1 == n){
-                            return true;
-                        }
-                        freq[s2[st]]++;
-                        if(freq[s2[st]]>0){
-                            count++;
-                        }
-                        st++;
+            if (count == 0) {
+
+                while (count == 0) {
+                    if (en - st + 1 == n) { // we need to check at every shrink if whether the valid window appeared maybe the valid window is from 3 to 6 so if we dont check it here while shrinking it will be checked from 0 to 6 and that will definitely fail.
+                        return true;
                     }
-                
+                    freq[s2[st]]++;
+                    if (freq[s2[st]] > 0) {
+                        count++;
+                    }
+                    st++;
+                }
             }
             en++;
         }
