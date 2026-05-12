51cc3f0dbf9340cfb08095a9fae5fa3338fa9195
SONALISINHA01 <149989109+SONALISINHA01@users.noreply.github.com>
2026-05-13 01:28:35 +0530

    Time: 0 ms (100%), Space: 18.1 MB (27.87%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -1,11 +1,27 @@
+// O(log n) average, O(n) worst due to duplicates
 class Solution {
 public:
     bool search(vector<int>& nums, int target) {
-        for(int i =0;i<nums.size();i++){
-            if(nums[i]==target){
-                return 1;
+        int l = 0, r = nums.size() - 1;
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+            if (nums[m] == target)
+                return true; // can't decide sorted half, skip both ends
+            if (nums[l] == nums[m] && nums[m] == nums[r]) {
+                l++;
+                r--;
+            } else if (nums[l] <= nums[m]) {
+                if (target >= nums[l] && target < nums[m])
+                    r = m - 1;
+                else
+                    l = m + 1;
+            } else {
+                if (target > nums[m] && target <= nums[r])
+                    l = m + 1;
+                else
+                    r = m - 1;
             }
         }
-        return 0;
+        return false;
     }
 };
