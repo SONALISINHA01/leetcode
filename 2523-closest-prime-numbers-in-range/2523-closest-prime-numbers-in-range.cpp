8bcea89319c6ff60ad1686cd8b42d53d74d9d56a
Sonali Sinha <149989109+SONALISINHA01@users.noreply.github.com>
2026-07-06 11:19:14 +0530

    Time: 234 ms (34.22%), Space: 23.5 MB (22.61%) - LeetHub

--- 2523-closest-prime-numbers-in-range/2523-closest-prime-numbers-in-range.cpp
+++ 2523-closest-prime-numbers-in-range/2523-closest-prime-numbers-in-range.cpp
@@ -1,43 +1,33 @@
 class Solution {
 public:
     vector<int> closestPrimes(int left, int right) {
-        int n = right;
-        vector<bool> prime(n + 1, false);
-        prime[0] = true;
-        prime[1] = true;
-
-        for (int i = 2; i * i <= n; i++) {
-            if (!prime[i]) {
-                for (int j = i * i; j <= n; j += i) {
-                    prime[j] = true;
+        vector<bool> prime(right+1, true);
+        prime[0]=false;
+        prime[1]=false;
+        for(int i =2;i<=right;i++){
+            if(prime[i]){
+                for(long long j =(long long)i*i;j<=right;j+=i){
+                    prime[j]=false;
                 }
             }
         }
-
-        vector<pair<int, int>> ans;
-
-        for (int i = left; i <= right; i++) {
-            if (!prime[i]) {
-                int j = i + 1;
-                while (j <= right && prime[j]) {
-                    j++;
-                }
-                if (j > right) break;
-                ans.push_back({i, j});
+        vector<int> canbe;
+        for(int i = left;i<=right;i++){
+            if(prime[i]){
+                canbe.push_back(i);
             }
         }
-
-        int mi = INT_MAX;
-        int x = -1, y = -1;
-
-        for (int i = 0; i < ans.size(); i++) {
-            if ((ans[i].second - ans[i].first) < mi) {
-                mi = ans[i].second - ans[i].first;
-                x = ans[i].first;
-                y = ans[i].second;
+        if(canbe.size()<=1){
+            return {-1,-1};
+        }
+        int mi= INT_MAX, x=-1,y=-1;
+        for(int i=1;i<canbe.size();i++){
+            if(mi>(canbe[i]-canbe[i-1])){
+                mi = canbe[i]-canbe[i-1];
+                x=canbe[i-1];
+                y=canbe[i];
             }
         }
-
-        return {x, y};
+        return {x,y};
     }
 };
