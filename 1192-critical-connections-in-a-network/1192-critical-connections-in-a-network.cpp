6039138e22a2ff0a1cc2aaea9a411fcaf62e26b0
Sonali Sinha <149989109+SONALISINHA01@users.noreply.github.com>
2026-07-25 11:47:01 +0530

    Time: 142 ms (83.89%), Space: 167.4 MB (95.19%) - LeetHub

--- 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.cpp
+++ 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.cpp
@@ -1,35 +1,34 @@
 class Solution {
 public:
     int timer =1;
-    void dfs(vector<vector<int>> &adj,int tin[],int low[],vector<vector<int>> &bridges,int node,int parent,vector<bool>& visited){
-        visited[node]=true;
-        tin[node]=low[node]=timer;
+    void dfs(int node,int parent,auto &adj,auto &vis,auto& bridges,int t_in[],int low[]){
+        vis[node]=true;
+        t_in[node]=low[node]=timer;
         timer++;
-        for(int i=0;i<adj[node].size();i++){
-            if(adj[node][i]==parent)continue;
-            if(visited[adj[node][i]]==false){
-                dfs(adj,tin,low,bridges,adj[node][i],node,visited);
-                low[node]=min(low[node],low[adj[node][i]]);
-                if(low[adj[node][i]]>tin[node]){
-                    bridges.push_back({adj[node][i],node});
+        for(auto &it:adj[node]){
+            if(it==parent)continue;
+            if(vis[it]==false){
+                dfs(it,node,adj,vis,bridges,t_in,low);
+                low[node]=min(low[node],low[it]);
+                if(low[it]>t_in[node]){
+                    bridges.push_back({node,it});
                 }
             }else{
-                low[node]=min(low[node],low[adj[node][i]]);
+                low[node]=min(low[it],low[node]);
             }
         }
     }
     vector<vector<int>> criticalConnections(int n, vector<vector<int>>& con) {
         vector<vector<int>> adj(n);
-        for(int i =0;i<con.size();i++){
+        for(int i=0;i<con.size();i++){
             adj[con[i][0]].push_back(con[i][1]);
             adj[con[i][1]].push_back(con[i][0]);
         }
         vector<vector<int>> bridges;
-        int tin[n];
+        vector<bool> vis(n,false);
+        int t_in[n];
         int low[n];
-        vector<bool> visited(n,false);
-        dfs(adj,tin,low,bridges,0,-1,visited);
+        dfs(0,-1,adj,vis,bridges,t_in,low);
         return bridges;
-
     }
 };
