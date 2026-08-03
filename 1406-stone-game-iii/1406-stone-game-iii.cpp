f30996eebfafb402a411a79659ee84a6095113e8
Sonali Sinha <149989109+SONALISINHA01@users.noreply.github.com>
2026-08-03 23:44:06 +0530

    Time: 36 ms (48%), Space: 140.9 MB (52%) - LeetHub

---
+++ 1406-stone-game-iii/1406-stone-game-iii.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    //usedai
+    int solve(int i, vector<int>& stoneValue, vector<int>& dp) {
+        int n = stoneValue.size();
+
+        if (i >= n) return 0;
+
+        if (dp[i] != INT_MIN) return dp[i];
+
+        int ans = INT_MIN;
+        int take = 0;
+
+        for (int k = 0; k < 3 && i + k < n; k++) {
+            take += stoneValue[i + k];
+            ans = max(ans, take - solve(i + k + 1, stoneValue, dp));
+        }
+
+        return dp[i] = ans;
+    }
+
+    string stoneGameIII(vector<int>& stoneValue) {
+        int n = stoneValue.size();
+        vector<int> dp(n, INT_MIN);
+
+        int diff = solve(0, stoneValue, dp);
+
+        if (diff > 0) return "Alice";
+        if (diff < 0) return "Bob";
+        return "Tie";
+    }
+};
