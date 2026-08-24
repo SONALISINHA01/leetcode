dbdfe0cdfdcdb2bcc27e94932f068fa5d59ae47b
Sonali Sinha <149989109+SONALISINHA01@users.noreply.github.com>
2026-08-25 02:08:03 +0530

    Time: 3 ms (45.7%), Space: 24.2 MB (24.85%) - LeetHub

--- 0303-range-sum-query-immutable/0303-range-sum-query-immutable.cpp
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.cpp
@@ -4,28 +4,26 @@ class SegmentTree{
     SegmentTree(int n){
         tree.resize(4*n,0);
     }
-    void build(int index,int left, int right,vector<int>& nums){
+    void build(int index, int left, int right, vector<int> &nums){
         if(left==right){
             tree[index]=nums[left];
             return;
         }
-        int mid = (left+right)>>1;
+        int mid=(left+right)>>1;
         build(2*index+1,left,mid,nums);
         build(2*index+2,mid+1,right,nums);
         tree[index]=tree[2*index+1]+tree[2*index+2];
     }
-    int query(int ql,int qr,int index , int left, int right){
-        //no overlapping
-        if(ql> right || qr<left){
+    int query(int index,int ql,int qr,int left, int right){
+        if(ql>right || qr<left){
             return 0;
         }
-        // full overlapping
-        if(ql<= left &&  right<= qr){
+        if(ql<=left && qr>=right){
             return tree[index];
         }
-        // partial overlap
-        int mid =(left+right)>>1;
-        return query(ql,qr,2*index+1, left, mid)+query(ql,qr,2*index+2,mid+1,right);
+        //partial overlap
+        int mid = (left+right)>>1;
+        return query(2*index+1,ql,qr,left,mid)+query(2*index+2,ql,qr,mid+1,right);
     }
 };
 class NumArray {
@@ -33,13 +31,13 @@ public:
     SegmentTree* st;
     int n;
     NumArray(vector<int>& nums) {
-       this->n= nums.size();
-       st = new SegmentTree(n);
-       st->build(0,0,n-1,nums);
+        this->n = nums.size();
+        st = new SegmentTree(n);
+        st->build(0,0,n-1,nums);
     }
     
     int sumRange(int left, int right) {
-        return st->query(left,right,0,0,n-1);
+        return st->query(0,left,right,0,n-1);
     }
 };
 
