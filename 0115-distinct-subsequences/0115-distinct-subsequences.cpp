57be0f2c5dfb5388447768d4c784291443192095
Sonali Sinha <149989109+SONALISINHA01@users.noreply.github.com>
2026-09-07 04:05:12 +0530

    Time: 31 ms (53.25%), Space: 44.2 MB (31.16%) - LeetHub

---
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int numDistinct(string s, string t) {
+        int n = s.size(), m = t.size();
+        vector<vector<long long>> dp(n+1,vector<long long>(m+1,0));
+        for(int i =0;i<=n;i++){
+            dp[i][m]=1;
+        }
+        for(int i=n-1;i>=0;i--){
+            for(int j = m-1;j>=0;j--){
+                if(s[i]==t[j]){
+                    if(dp[i+1][j+1] > INT_MAX - dp[i+1][j]){
+                        dp[i][j] = INT_MAX;
+                    }
+                    else{
+                        dp[i][j] =dp[i+1][j+1] + dp[i+1][j];
+                    }
+                }else{
+                    dp[i][j]=dp[i+1][j];
+                }
+            }
+        }
+        return dp[0][0];
+    }
+};
